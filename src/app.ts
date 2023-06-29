25799fc38a61d2d0a5b29d06cf106b09896123e1
cristhian martinez <crifamari@gmail.com>
2023-06-28 20:30:34 -0500

    app.ts: define 2 endpoints, / , /home

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,24 @@ import  express  from "express";
 
 const app = express();
 
+/**routing: enrutamiento */
+//http://localhost:3000/
+
+app.get("/", ( req, res ) => {
+    const messeage = "bienbenido a la API  de tienda"
+    console.log( messeage );
+    
+   res.send( `<h1>${messeage}<h1>`);
+});
+//http://localhost:3000/home
+app.get ("/home", (req, res) => {
+    const namepage = "home";
+
+    console.log(namepage);
+    res.send (`<h1>${namepage}<h1>`);
+    
+})
+
 app.listen(3000, () =>{
     console.log("servidor lanzado en el puerto 3000 ");
     
