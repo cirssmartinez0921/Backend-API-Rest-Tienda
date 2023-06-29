39ff9d3ef4308f11a37e8ca26d8c22105424767d
cristhian martinez <crifamari@gmail.com>
2023-06-28 21:06:58 -0500

    index.router: Define ruta de users

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 
 import  express, {Request, Response}  from "express";
+import routeMain from "./routes";
+
 
 
 
@@ -9,19 +11,21 @@ const app = express();
 //http://localhost:3000/  (EndPoint)
 
 app.get("/", ( req: Request, res: Response ) => {
-    const messeage = "bienbenido a la API  de tienda"
+    const messeage: string = "bienbenido a la API  de tienda"
     console.log( messeage );
     
    res.send( `<h1>${messeage}<h1>`);
 });
 //http://localhost:3000/home    (EndPoint)
 app.get ("/home", (req, res) => {
-    const namepage = "home";
+    const namepage : string = "home";
 
     console.log(namepage);
     res.send (`<h1>${namepage}<h1>`);
     
 })
+/**Middleware */
+app.use("/", routeMain);
 
 app.listen(3000, () =>{
     console.log("servidor lanzado en el puerto 3000 ");
---
+++ src/routes/index.ts
@@ -0,0 +1,15 @@
+//**archivo central de enrutamiento */
+
+import { Router, Request, Response} from "express";
+
+const router = Router();
+
+router.get("/users", (req: Request, res: Response )=> {
+    const msg = " users";
+
+    console.log(msg);
+    res.send(`<h1>${msg}<h1>`)
+    
+} ) 
+
+export default router;
