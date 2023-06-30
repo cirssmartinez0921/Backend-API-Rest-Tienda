4c892f2cb8789e0de753930a91b511953a0871e3
cristhian martinez <crifamari@gmail.com>
2023-06-29 21:48:23 -0500

    vinculamos rutas al controlador del producto

--- src/routes/products.route.ts
+++ src/routes/products.route.ts
@@ -1,10 +1,14 @@
 //**archivo central de enrutamiento */
 
 import { Router, Request, Response} from "express";
-import {getProducts} from "../controllers/product.controller"
+import {createproduct, deleteproduct, getProduct, getProducts, updateproduct} from "../controllers/product.controller"
 
 const router = Router();
 // http://localhost:3000/api/products
-router.get("/", getProducts );
+router.get( "/", getProducts );
+router.get ( "/", getProduct );
+router.post( "/", createproduct );
+router.put ( "/", updateproduct );
+router.delete ( "/", deleteproduct );
 
-export default router;
+export default router; 
