9bd23020357c978418edf2ed7ff7d881e232ef52
cristhian martinez <crifamari@gmail.com>
2023-06-30 21:37:31 -0500

    mongo.config: agregamos al URI de mongo el nombre de la base de datos

--- src/config/mongo.config.ts
+++ src/config/mongo.config.ts
@@ -3,7 +3,7 @@ import { connect } from "mongoose";
 
 //**configuracion para la conexion to mongoDB */
 async function dbconnet () : Promise <void>  {
-   const DB_URI = "mongodb://127.0.0.1:27017";   
+   const DB_URI = "mongodb://127.0.0.1:27017/store";   
    await connect (DB_URI);
 }
 
